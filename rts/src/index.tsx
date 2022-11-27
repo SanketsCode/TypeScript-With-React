eb0386e1c40eaadc23b24a75f7702f2a5f96f792
sanket <programmersanket@gmail.com>
2022-11-27 17:33:27 +0530

    Understading typescript with redux

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,14 +1,25 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import EventComponent from "./events/EventComponent";
 import GuestList from "./state/GuestList";
-import UserSearch from "./state/UserSearch";
+// import UserSearch from "./state/UserSearch";
+import * as React from 'react';
+import UserSearch from "./refs/UserSearch";
+
 
 const App = () => {
     return (
         <div>
             {/* <GuestList /> */}
+            {/* <UserSearch /> */}
+            {/* <EventComponent /> */}
             <UserSearch />
         </div>
     )
 }
 
-ReactDOM.render(<App />,document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
