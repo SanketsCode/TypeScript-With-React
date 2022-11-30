b2234dcd45d635f9b7c8313645a16d1bf04812c7
sanket <programmersanket@gmail.com>
2022-11-30 19:10:48 +0530

    basic Typescript is Done

--- redux-ts/src/components/RepositoryList.tsx
+++ redux-ts/src/components/RepositoryList.tsx
@@ -20,8 +20,10 @@ export const RepositoryList : React.FC = () => {
             <input type="text" value={term} onChange={e =>  setTerm(e.target.value)} />
             <button type="submit">Search</button>
         </form>
+        {error && <h3>{error}</h3>}
+        {loading && <h3>Loading....</h3>}
         {
-            data && data.map((name : string) => <li>{name}</li> )
+           !error && !loading && data && data.map((name : string) => <li >{name}</li> )
         }
     </div>
 }
